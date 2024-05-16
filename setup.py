d1d9a4fcc1f9195adfd4adaa349fe22ffbd872f0
Rishiraj-singh7 <rsshekhawat795@gmail.com>
2024-05-16 17:22:43 +0530

    Project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Heath Care Chatbot',
+    version= '0.0.0',
+    author= 'Rishi Shekhawat',
+    author_email= 'rsshekhawat795@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
